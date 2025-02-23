50f8a271621b5a8341ecb6d3b9b46e6af46406ae
sarahschan <sarahschan@hotmail.com>
2025-02-23 22:07:40 +0800

    3 stage dockerfile for day32 class task done

--- day32/day32-class-taskx/src/models.ts
+++
@@ -1,9 +0,0 @@
-// Purchase Order Model
-export interface PurchaseOrder {
-    name: string
-    address: string
-    email: string
-    deliveryDate: string
-    availability: string[]
-    urgent: boolean
-}
