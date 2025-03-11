fd0f8d5591eb947c76dace4afa86244f9c56435b
sarahschan <sarahschan@hotmail.com>
2025-03-11 18:18:59 +0800

    added a very strange route guard, different from the one we've learned. Not sure why the one we learnt is unavailable even though the angular is running on a valid version. Anyway, it stops accessing /checkout through the url bar if the cart size is 0 or less

--- extra practice/2024/template/ecommerce/client/src/app/app-routing.module.ts
+++ extra practice/2024/template/ecommerce/client/src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
 import { CategoryComponent } from './components/category/category.component';
 import { ConfirmCheckoutComponent } from './components/confirm-checkout/confirm-checkout.component';
+import { canActivateCheckout } from './route-guard.service';
 
 const routes: Routes = [
   { path: "", component: MainComponent },
   { path: "category/:category", component: CategoryComponent },
-  { path: "checkout", component: ConfirmCheckoutComponent},
+  { path: "checkout", 
+      component: ConfirmCheckoutComponent, 
+      canActivate: [canActivateCheckout]},
   { path: "**", redirectTo: ""}
 ];
 
---
+++ extra practice/2024/template/ecommerce/client/src/app/route-guard.service.ts
@@ -0,0 +1,55 @@
+import { inject, Injectable, OnInit } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, CanActivateFn, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CartStore } from './cart.store';
+import { map, Observable, Subscription, take } from 'rxjs';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class RouteGuardService implements CanActivate {
+
+//   private router = inject(Router);
+//   private cartStore = inject(CartStore);
+  
+//   canActivate(
+//     route: ActivatedRouteSnapshot, 
+//     state: RouterStateSnapshot
+//   ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+//     // If cart has 0 or less items, do not allow routing to page /checkout
+//     return this.cartStore.numProductsInCart$.pipe(
+//       take(1),
+//       map(itemCount => {
+//         if (itemCount > 0) {
+//           // Cart has items, allow navigation to checkout
+//           return true;
+//         } else {
+//           // Cart is empty, redirect to home page
+//           alert('Your cart is empty. Please add some items before proceeding to checkout.');
+//           return this.router.createUrlTree(['/']);
+//         }
+//       })
+//     );
+//   }
+// }
+
+// Create a functional guard instead of a class
+export const canActivateCheckout: CanActivateFn = (route, state) => {
+  
+  const router = inject(Router);
+  const cartStore = inject(CartStore);
+
+  // If cart has 0 or less items, do not allow routing to page /checkout
+  return cartStore.numProductsInCart$.pipe(
+    take(1),
+    map(itemCount => {
+      if (itemCount > 0) {
+        // Cart has items, allow navigation to checkout
+        return true;
+      } else {
+        // Cart is empty, redirect to home page
+        alert('Your cart is empty. Please add some items before proceeding to checkout.');
+        return router.createUrlTree(['/']);
+      }
+    })
+  );
+};
